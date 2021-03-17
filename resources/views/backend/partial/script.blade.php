a3a69f7fd84f97d74a7f123209577571800f0992
Jahid101 <jahidhasananik.official@gmail.com>
2021-03-17 17:51:47 +0600

    dropdown added

--- resources/views/backend/partial/script.blade.php
+++ resources/views/backend/partial/script.blade.php
@@ -9,3 +9,6 @@
         integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous">
     </script>
     <script src="dashboard.js"></script>
+
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
